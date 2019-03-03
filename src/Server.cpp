f165efb698a753a608d6a7861b3d20d78c324006
Vadim Cherenev <sl@anhot.ru>
2019-03-03 15:47:04 +0500

    Minor fix

--- src/Server.cpp
+++ src/Server.cpp
@@ -1,22 +1,15 @@
 #include <Server.h>
 
-bool _help_requested = false;
-
 int ServerApp::main(const vector<string> &)
 {
-    if (_help_requested)
-        return Application::EXIT_OK;
-
-    cerr << "Server started" << endl;
-
     TaskManager tm;
+    cerr << "Server started" << endl;
     try {
         tm.start(new MQTTTask());
     } catch (int e) {
         cerr << e << endl;
     }
     waitForTerminationRequest();
-
     tm.cancelAll();
     tm.joinAll();
 
